1ef1acbc5e0fe3d1be24311031a955dc99cc7439
Panin <panin1970@gmail.com>
2021-12-28 13:55:08 +0300

    data manager

---
+++ PersonList/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  PersonList
+//
+//  Created by Александр Панин on 28.12.2021.
+//
+
+import Foundation
---
+++ PersonList/Servises/DataManager.swift
@@ -0,0 +1,43 @@
+//
+//  DataManager.swift
+//  PersonList
+//
+//  Created by Александр Панин on 28.12.2021.
+//
+
+import Foundation
+
+class DataMansger {
+    
+    static let shared = DataMansger()
+    
+    let names = [
+    "John", "Aaaron", "Tim",
+    "Ted", "Steven", "Ivan",
+    "Alex", "Nicola", "Maria",
+    "Anna"
+    ]
+    
+    let surnames = [
+        "Smith", "Dow", "Tramp",
+        "Obama","Black", "Andersson",
+        "Gilmor", "Murphy", "Tolstoy",
+        "Popov"
+    ]
+    
+    let emails = [
+    "aaa@ya.ru", "bbb@ya.ru", "ccc@ya.ru",
+    "ddd@ya.ru", "eee@ya.ru", "fff@ya.ru",
+    "ttt@ya.ru", "sss@ya.ru", "rrr@ya.ru",
+    "uuu@ya.ru"
+    ]
+    
+    let phones = [
+    "134579874", "245893146", "321856841",
+    "421387690", "532097789", "612309877",
+    "744526531", "863521709", "912346237",
+    "037321881"
+    ]
+    
+    private init() {}
+}
